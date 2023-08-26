db19a5031c98d963f259c77dabad36b1f6ab30fe
libaice <libaice147@gmail.com>
2023-08-27 00:08:47 +0800

    init commit

---
+++ app/lens-provider.tsx
@@ -0,0 +1,18 @@
+import { Fuzzy_Bubbles } from "next/font/google";
+import React from "react";
+import { polygonMumbai, polygon } from "wagmi/chains";
+import {configureChains,createConfig,WagmiConfig} from "wagmi"
+
+
+// const {} = 
+
+// const config = createConfig({
+//     autoConnect: true,
+
+// })
+
+// export default function Provider({ children }: { children: React.ReactNode }) {
+//     return (
+//         <WagmiConfig config={}></WagmiConfig>
+//     )
+// }
